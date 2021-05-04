50af436a62c072efc04270f8686de29a2c79d4f9
KMS74 <happy.birthday74@hotmail.com>
2021-05-04 17:12:14 +0700

    first commit

---
+++ template_src/readme.md
@@ -0,0 +1,16 @@
+## Materialize Cordova Template
+
+### Create Project
+- cordova create project_name you.company.projectId app_name --template https://github.com/Komsan74/materialize-cordova-template
+
+### Platform
+- cordova platform add android@8
+
+### Plugins
+- cordova plugin add cordova-plugin-splashscreen
+- cordova plugin add cordova-plugin-x-toast
+
+### Build
+- cordova clean
+- cordova run
+- cordova build android
---
+++ template_src/www/js/index.js
@@ -0,0 +1,52 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+// Wait for the deviceready event before using any of Cordova's device APIs.
+// See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
+document.addEventListener('deviceready', onDeviceReady, false);
+
+function onDeviceReady() {
+    // Cordova is now initialized. Have fun!
+    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
+    document.getElementById('deviceready').classList.add('ready');
+}
+
+// back button to exit use cordova-plugin-x-toast
+document.addEventListener("backbutton", onBackKeyDown, false);
+var lastTimeBackPress=0;
+var timePeriodToExit=2000;
+
+function onBackKeyDown(e){
+    e.preventDefault();
+    e.stopPropagation();
+    if(new Date().getTime() - lastTimeBackPress < timePeriodToExit){
+        navigator.app.exitApp();
+    }else{
+        window.plugins.toast.showWithOptions(
+            {
+              message: "Press again to exit.",
+              duration: "short", // which is 2000 ms. "long" is 4000. Or specify the nr of ms yourself.
+              position: "bottom",
+              addPixelsY: -40  // added a negative value to move it up a bit (default 0)
+            }
+          );
+
+        lastTimeBackPress=new Date().getTime();
+    }
+};
