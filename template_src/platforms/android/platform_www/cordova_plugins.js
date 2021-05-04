ebce1e2e47a288439cae3d7082002e5cd4578ff3
KMS74 <happy.birthday74@hotmail.com>
2021-05-04 17:17:57 +0700

    set template

---
+++ template_src/platforms/android/platform_www/cordova_plugins.js
@@ -0,0 +1,25 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+  module.exports = [
+    {
+      "id": "cordova-plugin-splashscreen.SplashScreen",
+      "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
+      "pluginId": "cordova-plugin-splashscreen",
+      "clobbers": [
+        "navigator.splashscreen"
+      ]
+    },
+    {
+      "id": "cordova-plugin-x-toast.Toast",
+      "file": "plugins/cordova-plugin-x-toast/www/Toast.js",
+      "pluginId": "cordova-plugin-x-toast",
+      "clobbers": [
+        "window.plugins.toast"
+      ]
+    }
+  ];
+  module.exports.metadata = {
+    "cordova-plugin-whitelist": "1.3.4",
+    "cordova-plugin-splashscreen": "6.0.0",
+    "cordova-plugin-x-toast": "2.7.2"
+  };
+});
